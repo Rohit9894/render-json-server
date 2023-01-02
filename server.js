486882da34bf5fda596c05966d206d8cc17d5ca9
Rohit Gupta <princerohit9893@gmail.com>
2023-01-03 03:25:57 +0530

    update

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+const PORT = process.env.PORT || 3001;
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use("/api", router);
+server.listen(PORT, () =>
+  console.log(`JSON Server is running on port ${PORT}`)
+);
